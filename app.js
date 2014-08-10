7912d069aae4ed51c2aa342134e38ca36e8dae3e
amethyst <amethystcrafter@gmail.com>
2014-08-10 16:53:21 +0900

    2014-08-10

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express');
+var engine = require('ejs-locals')
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
@@ -12,9 +13,10 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.engine('ejs', engine);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -23,6 +25,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -30,6 +33,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/', routes.index);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
